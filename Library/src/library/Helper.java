fe5af53755fa969af8312235868d84303eedc084
bclarke <bclarke@RM1012-19-Java.fasbaldoyle.ie>
2013-07-11 08:59:15 +0100

    Print do not work !!!

---
+++ Library/src/library/Helper.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package library;
+
+/**
+ *
+ * @author bclarke
+ */
+public class Helper {
+    System.out.println("hi");
+}
